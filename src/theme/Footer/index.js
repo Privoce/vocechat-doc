6e366d2d8b211cc9900ccc5243af7840423dd2f0
Tristan Yang <yanggc888@163.com>
2022-11-08 12:13:22 +0800

    feat: add vocechat widget

---
+++ src/theme/Footer/index.js
@@ -0,0 +1,22 @@
+import React, { useEffect } from "react";
+import Footer from "@theme-original/Footer";
+
+export default function FooterWrapper(props) {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.setAttribute("data-origin", "https://privoce.voce.chat");
+    script.src = "https://privoce.voce.chat/widget.js";
+    script.async = true;
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+  return (
+    <>
+      <Footer {...props} />
+    </>
+  );
+}
